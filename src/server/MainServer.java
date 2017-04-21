f14ce019734471d18703dcec23f60d3d77fa3f1f
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2017-04-21 15:44:24 -0300

    Server host as argument

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -1,19 +1,40 @@
 package server;
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class MainServer 
 {
-	public static void RunServer(String serverName)
+	public static void RunServer(String serverName, String registryHost)
 	{
 		try
 		{
 			PartRepository pr = new PartRepository();
 			IPartRepository repo = (IPartRepository) UnicastRemoteObject.exportObject(pr, 0);
 			
-			Registry registry = LocateRegistry.createRegistry(1099);
-			registry.bind(serverName, repo);
+			Registry registry;
+			try
+			{
+				registry = LocateRegistry.getRegistry(registryHost, 1099);
+			}
+			catch(RemoteException e)
+			{
+				System.out.println("Could not locate remote registry, creating a new local one!");
+				registry = LocateRegistry.createRegistry(1099);
+			}
+	
+			
+			try
+			{
+				registry.bind(serverName, repo);
+			}
+			catch (AlreadyBoundException e)
+			{
+				System.out.println("An existent repository already uses the same name, please use a different name!");
+				return;
+			}
 			
             System.err.println("Server ready");
 		}
@@ -28,12 +49,18 @@ public class MainServer
 	public static void main(String[] args) 
 	{
 		String serverName = "IPartRepository";
+		String registryHost = "127.0.0.1";
 		
 		if(args.length >= 1)
 		{
 			serverName = args[0];
 		}
 		
-		RunServer(serverName);
+		if(args.length >= 2)
+		{
+			registryHost = args[1];
+		}
+		
+		RunServer(serverName, registryHost);
 	}
 }
